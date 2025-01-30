01ae0db53c0de2bade690dcb9b7cb3926f36c882
luisrocedev <luisilustraciones@gmail.com>
2025-01-30 14:38:33 +0100

    1

--- 027-hero dinamico/modulos/bienvenida/bienvenida.php
+++
@@ -1,9 +0,0 @@
-<script>
-    <?php include "../hero/hero.php"; ?>
-</script>
-<section id="bienvenida">
-    <h1>Bienvenidos a TaronjaBox</h1>
-    <p>Tu lugar de crossfit en valencia.</p>
-    <a href="modulos/quienes-somos/quienes-somos.php" class="btn">Quienes somos</a>
-</section>
-<link rel="stylesheet" href="modulos/bienvenida/bienvenida.css">
--- 027-hero dinamico/modulos/hero/hero.php
+++
@@ -1,4 +0,0 @@
-<section id="hero" data-seccion="<?php echo basename(__DIR__); ?>"></section>
-
-<!-- Incluir el JavaScript del Hero -->
-<script src="/modulos/hero/hero.js"></script>
