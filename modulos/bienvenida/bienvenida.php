638f070eab76263c3057ed3495ee5636a34aae24
luisrocedev <luisilustraciones@gmail.com>
2025-02-13 18:40:57 +0100

    1

--- modulos/bienvenida/bienvenida.php
+++ modulos/bienvenida/bienvenida.php
@@ -1,7 +1,12 @@
-<section id="bienvenida">
+<?php
+// Ruta dinámica de la imagen
+$backgroundImage = 'modulos/bienvenida/img/fondo.jpg';
+?>
+
+<section id="bienvenida" style="background-image: url('<?php echo $backgroundImage; ?>');">
     <div class="overlay">
-        <h1>Bienvenidos a <span>TaronjaBox</span></h1>
-        <p>Tu lugar de CrossFit en Valencia, donde la fuerza se encuentra con la pasión.</p>
+        <h1 class="hero-title">Bienvenidos a <span>TaronjaBox</span></h1>
+        <p class="hero-subtitle">Tu lugar de CrossFit en Valencia, donde la fuerza y la pasión se fusionan.</p>
         <a href="modulos/quienes-somos/quienes-somos.php" class="btn">Descúbrenos</a>
     </div>
 </section>
