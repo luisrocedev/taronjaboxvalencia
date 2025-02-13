150d696c614f97dd192de5b9b016a6a72ac44930
luisrocedev <luisilustraciones@gmail.com>
2025-02-13 18:21:26 +0100

    1

--- modulos/bienvenida/bienvenida.php
+++ modulos/bienvenida/bienvenida.php
@@ -2,7 +2,7 @@
     <div class="overlay">
         <h1>Bienvenidos a <span>TaronjaBox</span></h1>
         <p>Tu lugar de CrossFit en Valencia, donde la fuerza se encuentra con la pasión.</p>
-        <a href="modulos/quienes-somos/quienes-somos.php" class="btn">Conócenos</a>
+        <a href="modulos/quienes-somos/quienes-somos.php" class="btn">Descúbrenos</a>
     </div>
 </section>
 
